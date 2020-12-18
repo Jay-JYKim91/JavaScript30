a18feb9c0e86704a8e6bfe0643dfdcf2d01c2a2b
Juyeon Kim <jay.jykim91@gmail.com>
2020-12-18 20:47:06 +0900

    give up, not working(favicon error), not finished yet

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,39 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      // video.srcObject = localMediaStream;
+      // video.play();
+      // video.src = window.URL.createObjectURL(localMediaStream);
+      // video.play();
+    })
+    .catch(error => {
+      console.error(`Noooo!`, error);
+    });
+};
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHegith;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  console.log(data);
+
+}
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
